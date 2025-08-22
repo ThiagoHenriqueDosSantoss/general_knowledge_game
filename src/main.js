37cc5ac68120de4d70b41c5403d8db847c32abe2
Thiago Henrique <thiago.1983@aluno.pr.senac.br>
2025-08-22 19:43:03 -0300

    Feat: open trivia api

--- src/main.js
+++ src/main.js
@@ -1,25 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.mount('#app')
+.use(VueAxios, axios)
 
-export default{
-    name:'Axios',
-    methods: {
-        /**
-         Este metodo realiza a requisição para a API do Open Trivia,
-         e  retorna os dados com as perguntas;
-         */
-        getInfo() {
-            axios
-            .get("https://opentdb.com/api.php?amount=10&category=18")
-            .then((res) => {
-                console.log(res.data)
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        }
-    }
-}
+
